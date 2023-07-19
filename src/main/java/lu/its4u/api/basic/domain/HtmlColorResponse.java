31442c7a9bc4aaee043076e0a885d710c52ae338
Olivier Castanos <olivier.castanos@its4u.com>
2023-07-19 11:57:55 +0200

    update with logs

---
+++ src/main/java/lu/its4u/api/basic/domain/HtmlColorResponse.java
@@ -0,0 +1,15 @@
+package lu.its4u.api.basic.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HtmlColorResponse {
+	private String color;
+
+}
---
+++ src/main/java/lu/its4u/api/main/controller/Helper.java
@@ -0,0 +1,6 @@
+package lu.its4u.api.main.controller;
+
+public interface Helper {
+	final char[] hexDigits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+}
