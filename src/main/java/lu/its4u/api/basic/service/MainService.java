990558bc90d7f1c56b890c68c21409b803f5711b
Olivier Castanos <olivier.castanos@its4u.com>
2023-07-19 15:55:22 +0200

    update

--- src/main/java/lu/its4u/api/basic/service/MainService.java
+++ src/main/java/lu/its4u/api/basic/service/MainService.java
@@ -27,7 +27,7 @@ public class MainService {
 	}
 
 	public int purge() {
-		long delta = 1 * 1000 * 60 * 5;
+		long delta = 1 * 1000 * 60 * 1;
 		List<ApiCallEntity> list = this.repository.findByCallDateBefore(new Date(System.currentTimeMillis() - delta));
 		this.repository.deleteAll(list);
 		return list.size();
